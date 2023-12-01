1ddf87ccceec5b812b46582641b4c63022469b7b
Luana Siqueira Ronchi <luana.sronchi@gmail.com>
2023-12-01 18:17:34 -0300

    Update files

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,9 +1,12 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <stdio.h>
+
 
 // Rescrever endereÃ§o MAC com o do emissor da mensagem
 uint8_t broadcastAddress[] = { 0xD8, 0xBC, 0x38, 0xE2, 0x48, 0x9C };
 String Buffer[9];
+int j;
 // Estrutura example para enviar dados
 // Must match the receiver structure
 typedef struct struct_message {
@@ -39,10 +42,12 @@ void RecebeDados(const uint8_t *mac, const uint8_t *incomingData, int len) {
   for (int i = 0; i < 9; i++) {
     Serial.print("Buffer:   ");
     Serial.println(Buffer[j]);
-    if (Buffer[i] == null) {
+    if (Buffer[i] != "a") {
 
       Buffer[i] = myData.R;
-      Serial.print("Valor inserido na %dº posição do Buffer:   ", i);
+      Serial.print("Valor inserido na ");
+      Serial.print(i);
+      Serial.print(" posição do Buffer:   ");
       Serial.println(Buffer[i]);
       break;
     }
